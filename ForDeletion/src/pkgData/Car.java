a5f532ff1e1a84f795d3a3203b5c1449bdd5f0a2
schueler <schueler@ubuntu>
2018-06-10 23:44:18 -0700

    second

--- ForDeletion/src/pkgData/Car.java
+++ ForDeletion/src/pkgData/Car.java
@@ -7,13 +7,17 @@ public class Car {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 }
