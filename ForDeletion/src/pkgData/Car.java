bdef4253f58bef97d7e2fe74921c6af1b98dabcb
schueler <schueler@ubuntu>
2018-06-10 23:41:56 -0700

    commit car

--- ForDeletion/src/pkgData/Car.java
+++ ForDeletion/src/pkgData/Car.java
@@ -4,5 +4,16 @@ public class Car {
 	private String name;
 	private int id;
 	
-	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 }
